d2e3989d5eb3ec870d95523eff1cdb0812f7ee69
tomoe.kawabe <cm9mhxki11pf6@gmail.com>
2020-04-12 15:49:52 +0900

    Modify Index/NewServlet

--- src/controllers/ShowServlet.java
+++ src/controllers/ShowServlet.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,19 +11,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
-
-
+import utils.DBUtil;
 /**
- * Servlet implementation class NewServlet
+ * Servlet implementation class ShowServlet
  */
-@WebServlet("/new")
-public class NewServlet extends HttpServlet {
+@WebServlet("/show")
+public class ShowServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public NewServlet() {
+    public ShowServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -31,16 +31,17 @@ public class NewServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("_token", request.getSession().getId());
+        // TODO Auto-generated method stub
+        EntityManager em =DBUtil.createEntityManager();
 
-        request.setAttribute("message", new Message());
+        Message m = em.find(Message.class, Integer.parseInt(request.getParameter("id")));
 
-        RequestDispatcher rd =request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
-        rd.forward(request,response);
+        em.close();
 
+        request.setAttribute("message", m);
 
-
-     }
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/show.jsp");
+        rd.forward(request, response);
     }
 
-
+}
