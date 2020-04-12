7031a439b2d84f7ad62a5e706a1959fdadbdddb0
tomoe.kawabe <cm9mhxki11pf6@gmail.com>
2020-04-12 19:51:46 +0900

    Add validation

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import models.Message;
 import utils.DBUtil;
+import validators.MessageValidator;
 
 /**
  * Servlet implementation class CreateServlet
@@ -48,6 +51,19 @@ public class CreateServlet extends HttpServlet {
             m.setCreated_at(currentTime);
             m.setUpdated_at(currentTime);
 
+            List<String> errors = MessageValidator.validate(m);
+            if(errors.size() > 0) {
+              em.close();
+
+              request.setAttribute("_token", request.getSession().getId());
+              request.setAttribute("message", m);
+              request.setAttribute("errors", errors);
+
+              RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+              rd.forward(request, response);
+
+            } else {
+
             em.getTransaction().begin();
             em.persist(m);
             em.getTransaction().commit();
@@ -55,6 +71,7 @@ public class CreateServlet extends HttpServlet {
             em.close();
 
             response.sendRedirect(request.getContextPath() + "/index");
+            }
         }
     }
 
